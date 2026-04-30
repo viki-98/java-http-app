af2a6eed0665f2071adc980a2353283cf7626d1a
viki-98 <vikipodolian@gmail.com>
2026-04-30 18:00:18 +0300

    1.3.0

--- src/main/java/org/post_hub/javahttpapp/HelloController.java
+++ src/main/java/org/post_hub/javahttpapp/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
     @GetMapping("/")
     public Map<String, String> root() {
         return Map.of(
-                "message", "Spring Boot app is running. testing jenkins",
+                "message", "Spring Boot app is running. testing jenkins 22",
                 "endpoint", "/"
         );
     }
